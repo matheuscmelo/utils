c2de074dce73cc9dc9c8e6445a31bede2d0a356b
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-04-30 11:41:43 -0300

    a

--- utils/work_distributer/requester.py
+++ utils/work_distributer/requester.py
@@ -6,15 +6,17 @@ from time import sleep
 
 class RefreshRequester(object):
 
-    def __init__(self, queue):
+    def __init__(self, queue, max_tries=5, timeout=15):
         self.redis = Redis(host='redis')
         self.queue = queue
+        self.max_tries = max_tries
+        self.timeout = 10
 
     def block_request(self, data):
         work_id = self._generate_work_id()
         data.update({"work_id": work_id})
 
-        for _ in range(5):
+        for _ in range(self.max_tries):
             if self._wait_for_receive(work_id, data):
                 response = self._wait_for_response(work_id)
                 if response:
@@ -30,9 +32,10 @@ class RefreshRequester(object):
         return False
 
     def _wait_for_response(self, work_id):
-        key, value = self.redis.brpop(work_id, timeout=30)
-
-        if value is not None:
+        response = self.redis.brpop(work_id, timeout=self.timeout)
+        if response is not None:
+            key, value = response
+        if value is not None and response is not None:
             data = json.loads(value.decode())
             return data
         else:
